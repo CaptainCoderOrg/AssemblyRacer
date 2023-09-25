300c7881206b6101d79b6361d673f2cabb624a9b
Joseph Collard <jcollard@users.noreply.github.com>
2023-09-25 15:36:46 -0700

    chore: remove debug logs.

--- Some Assembly Required/Assets/Scripts/PlayerDeckManager.cs
+++ Some Assembly Required/Assets/Scripts/PlayerDeckManager.cs
@@ -30,7 +30,6 @@ public class PlayerDeckManager : MonoBehaviour
 
     public void DrawCards(int count, System.Action onAnimationComplete)
     {
-        Debug.Log($"DrawCards({count})");
         if (count == 0) 
         { 
             onAnimationComplete.Invoke(); 
@@ -69,7 +68,6 @@ public class PlayerDeckManager : MonoBehaviour
     }
     public void DrawCard(System.Action onAnimationComplete)
     {
-        Debug.Log("In draw card");
         // TODO: Display out of cards message?
         if (PlayerDeck.Count == 0 && DiscardPile.Count == 0) { return; }
         if (PlayerDeck.Count == 0)
